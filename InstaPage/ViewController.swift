2366f47460ef01d074ec6406889cfde895360723
lus0401 <66708571+lus0401@users.noreply.github.com>
2024-01-31 15:51:43 +0900

    Initial Commit

---
+++ InstaPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPage
+//
+//  Created by t2023-m0051 on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
